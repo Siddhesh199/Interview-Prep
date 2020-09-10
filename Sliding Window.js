ca2f2121387bd12724655645815329f8e51a5093
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-10 18:45:51 +0530

    Sliding Window

--- Sliding Window.js
+++ Sliding Window.js
@@ -41,7 +41,6 @@ function maxSubArraySum(arr, num){
     for(let i = 0; i < num; i++){
         maxSum += arr[i];
     }
-
     tempSum = maxSum;
     for(let i = num; i < arr.length; i++){
         tempSum = tempSum - arr[i - num] + arr[i];
