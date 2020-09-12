647f5c1e894a74aeb49f3df9f287ef5e842076eb
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-12 23:51:05 +0530

    Capitalize first

--- Recursion Problems/Capitalize first.js
+++ Recursion Problems/Capitalize first.js
@@ -5,15 +5,14 @@
 //capitalizeFirst(['car','taco','banana']); --> ['Car','Taco','Banana']
 
 
-function capitalizeWords (array) {
+function capitalizeFirst (array) {
     if (array.length === 1) {
-      let str = array[0];
-      return [str[0].toUpperCase() + str.substring(1)];
+      return [array[0][0].toUpperCase() + array[0].substr(1)];
     }
-    let res = capitalizeWords(array.slice(0, -1));
-    let str = array.slice(array.length-1)[0];
-    res.push(str[0].toUpperCase() + str.substring(1));
-    return res; 
-}
+    const res = capitalizeFirst(array.slice(0, -1));
+    const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+    res.push(string);
+    return res;
+  }
 
 
