589953ce5615c48351f555a84d84dfc3687fe7db
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-13 19:29:54 +0530

    Stringify numbers

---
+++ Recursion Problems/Stringify numbers.js
@@ -0,0 +1,52 @@
+//Write a function which takes in an object and finds all the values which are numbers and
+//converts them to strings.
+
+//Sample Input:
+/*
+let obj = {
+    num: 1,
+    test: [],
+    data: {
+        val: 4,
+        info: {
+            isRight: true,
+            random: 66
+        }
+    }
+}
+/*
+
+stringifyNumbers(obj)
+
+
+//Output: 
+/*
+{
+    num: "1",
+    test: [],
+    data: {
+        val: "4",
+        info: {
+            isRight: true,
+            random: "66"
+        }
+    }
+}
+*/
+
+
+function stringifyNumbers(obj) {
+    var newObj = {};
+    for (var key in obj) {
+      if (typeof obj[key] === 'number') {
+        newObj[key] = obj[key].toString();
+      } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+        newObj[key] = stringifyNumbers(obj[key]);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+    return newObj;
+}
+
+
