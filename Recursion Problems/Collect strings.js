523144bb688b6018652677702c021a3a00cbcc1a
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-13 19:35:32 +0530

    Collect strings

---
+++ Recursion Problems/Collect strings.js
@@ -0,0 +1,63 @@
+//Write a function which accepts an object and returns an array of all the values
+//in the object that have a typeof string.
+
+
+//Sample Input:
+/*
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+*/
+
+//collectStrings(obj) --> ["foo", "bar", "baz"])
+
+
+
+//Solution 1: Helper method recursion
+function collectStrings(obj) {
+    var stringsArr = [];
+ 
+    function gatherStrings(o) {
+        for(var key in o) {
+            if(typeof o[key] === 'string') {
+                stringsArr.push(o[key]);
+            }
+            else if(typeof o[key] === 'object') {
+                return gatherStrings(o[key]);
+            }
+        }
+    }
+ 
+    gatherStrings(obj);
+ 
+    return stringsArr;
+}
+
+
+
+//Solution 2: Pure recursion
+function collectStrings(obj) {
+    var stringsArr = [];
+    for(var key in obj) {
+        if(typeof obj[key] === 'string') {
+            stringsArr.push(obj[key]);
+        }
+        else if(typeof obj[key] === 'object') {
+            stringsArr = stringsArr.concat(collectStrings(obj[key]));
+        }
+    }
+ 
+    return stringsArr;
+}
