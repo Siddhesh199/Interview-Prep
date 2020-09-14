08799c630ab282a8f05dd46019ae3adea2d11964
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-14 20:05:22 +0530

    Bubble sort

---
+++ Sorting Algorithms/Bubble sort.js
@@ -0,0 +1,31 @@
+//Implement Bubble Sort
+
+//Sample Input:
+//bubbleSort([22, 4, 1, 3, 9, 8, 21]) --> [1, 3, 4, 8, 9, 21, 22]
+
+
+function bubbleSort(arr){
+    
+    const swap = (arr, idx1, idx2) => {
+         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    };
+
+    let noSwaps;
+
+    for(let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j + 1]);
+            if(arr[j] > arr[j + 1]){
+                swap(arr, j, j + 1);
+                noSwaps = false;
+            }
+        }
+
+        if(noSwaps) break;  
+    }
+
+    return arr;
+}
+
+
