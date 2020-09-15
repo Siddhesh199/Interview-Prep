846f0e4873992d712250e417272625921ed40df5
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-15 13:18:14 +0530

    Insertion sort

---
+++ Sorting Algorithms/Insertion sort.js
@@ -0,0 +1,20 @@
+//Implement Insertion Sort
+
+//Sample Input:
+//insertionSort([22, 4, 1, 3, 9, 8, 21]) --> [1, 3, 4, 8, 9, 21, 22]
+
+
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let currentVal = arr[i];
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j+1] = arr[j];
+        }
+
+        arr[j+1] = currentVal;
+    }
+
+    return arr;
+}
+
+
