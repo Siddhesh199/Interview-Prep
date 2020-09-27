877a41034bf7965373c12b191c6c7e078a8253a5
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-27 20:12:45 +0530

    Stack

---
+++ Data Structures/Stack.js
@@ -0,0 +1,44 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    //Adds a node at the end
+    //Time complexity - O(1)
+    push(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+
+    //Removes a node from the end
+    //Time complexity - O(1)
+    pop(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+}
+
+
