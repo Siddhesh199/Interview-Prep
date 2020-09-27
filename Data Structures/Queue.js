c292da1fc83f618049b8fccd4a648b2ec09d101c
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-09-27 22:30:55 +0530

    Queue

---
+++ Data Structures/Queue.js
@@ -0,0 +1,45 @@
+//Queue
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    //Adds a node at the end
+    //Time complexity - O(1)
+    enqueue(val){
+        let newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    //Removes a node from the beginning
+    //Time complexity - O(1)
+    dequeue(){
+        if(!this.first) return null;
+        let temp = this.first;
+        if(this.first === this.last){
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp.val;
+    }
+}
+
+
