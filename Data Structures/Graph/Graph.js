5de32cbe023e48f876858f931237ea3b7c5eb415
Siddhesh199 <siddheshsalaskar188@gmail.com>
2020-10-02 17:24:39 +0530

    Graph Traversal Algorithms

---
+++ Data Structures/Graph/Graph.js
@@ -0,0 +1,109 @@
+//Unweighted Graph
+
+class Graph{
+    constructor(){
+        this.adjacencyList = {};
+    }
+
+    //Adds a vertex in the graph
+    addVertex(vertex){
+        if(!this.adjacencyList[vertex]){
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    //Adds an undirected edge between the two vertices
+    addEdge(v1, v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+
+    //Removes the edge between the two vertices
+    removeEdge(v1, v2){
+        this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v => v !== v2
+        );
+        this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v => v !== v1
+        );
+    }
+
+    //Removes the vertex and the edges connected to it
+    removeVertex(vertex){
+        while(this.adjacencyList[vertex].length){
+            const adjacentVertex = this.adjacencyList[vertex].pop();
+            this.removeEdge(vertex, adjacentVertex);
+        }
+        delete this.adjacencyList[vertex];
+    }
+
+
+    //Graph Traversal Algorithms
+
+    //Depth First Traversal Recursive
+    DFTRecursive(start){
+        const result = [];
+        const visited = {};
+        const adjacencyList = this.adjacencyList;
+
+        (function dfs(vertex){
+            if(!vertex) return null;
+            visited[vertex] = true;
+            result.push(vertex);
+            adjacencyList[vertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    return dfs(neighbor);
+                }
+            });
+        })(start);
+
+        return result;
+    }
+
+    //Depth First Traversal Iterative
+    DFTIterative(start){
+        const stack = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+
+        visited[start] = true;
+        while(stack.length){
+            currentVertex = stack.pop();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
+
+    //Breadth First Traversal
+    BFT(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        
+        return result;
+    }
+}
+
+
